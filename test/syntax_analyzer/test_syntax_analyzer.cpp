e451eb4dc00a0235380ab7d52f286a7e93ba07a5
Sergey Belov <serge_belov@ukr.net>
2020-07-24 19:16:50 +0300

    Fix unit test crash.

--- test/syntax_analyzer/test_syntax_analyzer.cpp
+++ test/syntax_analyzer/test_syntax_analyzer.cpp
@@ -23,13 +23,27 @@ public:
     int i = 1;
     while (i < argc) {
       if (strcmp(argv[i], "--class") == 0) {
-        if (i + 1 <= argc) {
+        if (i + 1 >= argc) {
           std::cout << "CommandLineArgs: Empty class name!" << std::endl;
           return false;
         }
         className_ = argv[i + 1];
+        i+=2;
+      } else if (strcmp(argv[i], "--testCase") == 0) {
+        if (i + 1 >= argc) {
+          std::cout << "CommandLineArgs: Empty test case name!" << std::endl;
+          return false;
+        }
+        testCase_ = argv[i + 1];
+        i += 2;
       }
     }
+
+    if (className_ ==  nullptr || testCase_ == nullptr) {
+      return false;
+    }
+
+    return true;
   }
 
 private:
@@ -44,12 +58,19 @@ void printHelp() {
 
 }
 
+int TestCaseAdd(SyntaxAnalyzer* sa) {
+  return 1;
+}
+
 int main(int argc, char* argv[]) {
   CommandLineArgs commandLineArgs;
   if (commandLineArgs.init(argc, argv) == false) {
     printHelp();
     return 1;
   }
+
+  std::cout << "className: " << commandLineArgs.className() << std::endl;
+  std::cout << "testCase : " << commandLineArgs.testCase() << std::endl;
 
   std::unique_ptr<SyntaxAnalyzer> syntax_analyzer;
   
@@ -60,5 +81,14 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  return 1;
+  int ret = 1;
+
+  if (strcmp(commandLineArgs.testCase(), "Add") == 0) {
+    ret = TestCaseAdd(syntax_analyzer.get());
+  } else {
+    std::cerr << "Unexpected test case: " << commandLineArgs.testCase() << std::endl;
+    return 1;
+  }
+
+  return ret;
 }
